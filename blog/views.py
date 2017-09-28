1dd4ca04c8a30da8a737ef8c73c0fc96ecc98fbf
Vivian Phung <vivianphung@Vivians-MacBook-Pro.local>
2017-09-28 16:27:48 -0700

    changes to edit pages

--- blog/views.py
+++ blog/views.py
@@ -34,12 +34,14 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
+            # (above) this is if you want it to publish right now otherwise it will be saved as a draft
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
