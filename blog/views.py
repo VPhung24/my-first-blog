d9101a2eeda1563e0e648d92a077bfb64d92c3da
Vivian Phung <vivianphung@Vivians-MacBook-Pro.local>
2017-09-28 19:19:27 -0700

    happening now

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,8 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponsePermanentRedirect
+from django.core.urlresolvers import reverse
 
 
 
