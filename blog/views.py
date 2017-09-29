d7fb75eea18b1ed755c8ba447ac98fe761c7c69b
Vivian Phung <vivianphung@Vivians-MacBook-Pro.local>
2017-09-29 00:24:34 -0700

    real

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponsePermanentRedirect
 from django.core.urlresolvers import reverse
 
-def post_list(request, pk):
+def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
